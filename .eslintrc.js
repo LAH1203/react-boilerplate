652f1ee1ee4028688e25c3b0bbd14b20f2dd73ce
이아현 <lah1203@naver.com>
2022-04-22 17:23:17 +0900

    eslint, prettier 설정

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: 'plugin:prettier/recommended',
+  plugins: 'prettier',
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module',
+  },
+  rules: {},
+};
